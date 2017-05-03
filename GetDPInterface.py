22ec9415e54c454ae2af40109ad63cde9e73165b
tobadavid <tdavid91@hotmail.com>
2017-05-03 13:30:20 +0200

    Extend GetDP interface for thermal coupling.

--- GetDPInterface.py
+++ GetDPInterface.py
@@ -24,34 +24,36 @@ class GetDPSolver(SolidSolver):
     Des.
     """
 
-    def __init__(self, testname, pythonFlag):
+    def __init__(self, testname, resolution, computationType, pythonFlag):
         """
         Des
         """
 
-	self.testname = testname
-	self.pythonFlag = pythonFlag
-	self.forceN = {}
-	self.dispN = {}
-	self.dispNm1 = {}
-	self.dispNm2 = {}
-	self.stateN = {}
-	self.stateNm1 = {}
-	self.currentDT = 1.0
+        self.testname = testname
+        self.pythonFlag = pythonFlag
+        self.computationType = computationType
+        self.resolution = resolution
+        self.forceN = {}
+        self.dispN = {}
+        self.dispNm1 = {}
+        self.dispNm2 = {}
+        self.stateN = {}
+        self.stateNm1 = {}
+        self.currentDT = 1.0
         self.pathToGetDP = "/home/dthomas/InstalledSoftware/GetDP/bin/getdp"
 
-	if self.pythonFlag:
+        if self.pythonFlag:
             import getdp
             GetDPSetNumber("Initialize", 1)
-            GetDP(["getdp", self.testname, "-solve", "structure"])
+            GetDP(["getdp", self.testname, "-solve", self.resolution])
             p = GetDPGetNumber("nodalPosition");
             self.dispNm2 = self.__vecToDic(GetDPGetNumber("nodalDisplacementNm2"))
             self.dispNm1 = self.__vecToDic(GetDPGetNumber("nodalDisplacementNm1"))
             self.dispN = self.__vecToDic(GetDPGetNumber("nodalDisplacement"))  
             initPos =  self.__vecToDic(GetDPGetNumber("nodalPosition"))
             initVel =  self.__vecToDic(GetDPGetNumber("nodalVelocity"))
-	else:
-            os.system(self.pathToGetDP +" {} -setnumber Initialize 1 -setnumber OutputFiles 1 -solve structure".format(self.testname))
+        else:
+            os.system(self.pathToGetDP +" {} -setnumber Initialize 1 -setnumber OutputFiles 1 -solve {}".format(self.testname, self.resolution))
             with open("nodalPosition.txt") as f:
                 strings = f.readlines()
             self.nNodes = len(strings)-1
@@ -81,10 +83,12 @@ class GetDPSolver(SolidSolver):
             self.nodalLoads_X = np.zeros(self.nPhysicalNodes)
             self.nodalLoads_Y = np.zeros(self.nPhysicalNodes)
             self.nodalLoads_Z = np.zeros(self.nPhysicalNodes)
+            self.__writeVecToFile("nodalHeatFlux.txt", np.zeros(self.nPhysicalNodes),  np.zeros(self.nPhysicalNodes), np.zeros(self.nPhysicalNodes), self.nodalInterfIndex)
             self.__setCurrentState()
             self.nodalVel_XNm1 = self.nodalVel_X.copy()
             self.nodalVel_YNm1 = self.nodalVel_Y.copy()
             self.nodalVel_ZNm1 = self.nodalVel_Z.copy()
+
         self.initRealTimeData()
 
     def __readIndex(self, fileName):
@@ -127,6 +131,24 @@ class GetDPSolver(SolidSolver):
 
         return (vec_x, vec_y, vec_z)
 
+    def __readFileToScal(self, fileName, nNodes):
+        """
+        Des
+        """
+
+        scal = np.zeros(nNodes)
+
+        with open(fileName) as f:
+            strings = f.readlines()
+
+        iVertex = 0
+        for iter in range(1, len(strings)):
+            no = int(strings[iter].split()[0])
+            scal[iVertex] = float(strings[iter].split()[1])
+            iVertex += 1
+
+        return scal
+
     def __writeVecToFile(self, fileName, vec_x, vec_y, vec_z, index):
         """
         Des.
@@ -138,6 +160,19 @@ class GetDPSolver(SolidSolver):
         for iVertex in range(nNodes):
             node = index[iVertex]
             f.write(str(node) + " "+ str(vec_x[iVertex]) + " " + str(vec_y[iVertex]) + " " + str(vec_z[iVertex]) + "\n")
+        f.close()
+
+    def __writeScalToFile(self, fileName, scal, index):
+        """
+        Des.
+        """
+
+        f = open(fileName, "w")
+        nNodes = scal.size
+        f.write(str(nNodes) + '\n')
+        for iVertex in range(nNodes):
+            node = index[iVertex]
+            f.write(str(node) + " " + str(scal[iVertex]) + "\n" )
         f.close()
 
     def run(self, t1, t2):
@@ -152,7 +187,10 @@ class GetDPSolver(SolidSolver):
         else:
             self.__writeVecToFile("nodalDisplacementNm1.txt", self.__nodalDomainDispNm1_X, self.__nodalDomainDispNm1_Y, self.__nodalDomainDispNm1_Z, self.nodalDomainIndex)
             self.__writeVecToFile("nodalDisplacementNm2.txt", self.__nodalDomainDispNm2_X, self.__nodalDomainDispNm2_Y, self.__nodalDomainDispNm2_Z, self.nodalDomainIndex)
-            os.system(self.pathToGetDP +" {} -setnumber Initialize 0 -setnumber OutputFiles 1 -setnumber T1 {} -setnumber T2 {} -solve structure".format(self.testname, t1, t2))
+            if self.computationType == 'unsteady':
+                os.system(self.pathToGetDP +" {} -setnumber Initialize 0 -setnumber OutputFiles 1 -setnumber T1 {} -setnumber T2 {} -solve {}".format(self.testname, t1, t2, self.resolution))
+            else:
+                os.system(self.pathToGetDP +" {} -setnumber Initialize 0 -setnumber OutputFiles 1 -solve {}".format(self.testname, self.resolution))
             self.__setCurrentState()
             
 
@@ -170,6 +208,10 @@ class GetDPSolver(SolidSolver):
         self.nodalVel_X, self.nodalVel_Y, self.nodalVel_Z = self.__readFileToVec("nodalVelocity.txt", self.nPhysicalNodes)
 
         self.__nodalDomainDisp_X, self.__nodalDomainDisp_Y, self.__nodalDomainDisp_Z = self.__readFileToVec("nodalDisplacement.txt", self.__nDomainNodes)
+
+        self.nodalHeatFlux_X , self.nodalHeatFlux_Y , self.nodalHeatFlux_Z = self.__readFileToVec("nodalHeatFlux.txt", self.nPhysicalNodes)
+
+        self.nodalTemperature = self.__readFileToScal("nodalTemperature.txt", self.nPhysicalNodes)
 
     def getNodalInitialPositions(self):
         """
@@ -192,6 +234,28 @@ class GetDPSolver(SolidSolver):
 
         self.__writeVecToFile("nodalForce.txt", load_X, load_Y, load_Z, self.nodalInterfIndex)
 
+
+    def applyNodalTemperatures(self, Temperature, val_time):
+        """
+        Des.
+        """
+
+        self.__writeScalToFile("nodalTemperature.txt" , Temperature, self.nodalInterfIndex)
+
+    def applyNodalNormalHeatFluxes(self, NormalHeatFlux, val_time):
+        """
+        Des.
+        """
+
+        self.__writeScalToFile("nodalNormalHeatFlux.txt", NormalHeatFlux, self.nodalInterfIndex)
+
+    def applyNodalHeatFluxes(self, HeatFlux_X, HeatFlux_Y, HeatFlux_Z, time):
+        """
+        Des.
+        """
+
+        self.__writeVecToFile("nodalHeatFlux.txt", HeatFlux_X, HeatFlux_Y, HeatFlux_Z)
+
     def update(self):
         """
         Des.
@@ -211,10 +275,10 @@ class GetDPSolver(SolidSolver):
         des.
         """
 
-        extractNode = 7
-        self.iVertexExtract = self.nodalInterfIndex.keys()[self.nodalInterfIndex.values().index(extractNode)]
-        solFile = open('extractPhysicalNode' + str(7) + '.ascii', "w")
-        solFile.write("Time\tnIter\tDx\tDy\tDz\tVx\tVy\tVz\n")
+        self.extractNode = 7        #should be decided by the user outside of the function
+        self.iVertexExtract = self.nodalInterfIndex.keys()[self.nodalInterfIndex.values().index(self.extractNode)]
+        solFile = open('extractPhysicalNode' + str(self.extractNode) + '.ascii', "w")
+        solFile.write("Time\tnIter\tDx\tDy\tDz\tVx\tVy\tVz\tT\tQx\tQy\tQz\n")
         solFile.close()
 
     def saveRealTimeData(self, time, nFSIIter):
@@ -222,8 +286,7 @@ class GetDPSolver(SolidSolver):
         des.
         """
 
-        extractNode = 7
-        solFile = open('extractPhysicalNode' + str(7) + '.ascii', "a")
+        solFile = open('extractPhysicalNode' + str(self.extractNode) + '.ascii', "a")
         
         Dx = self.nodalDisp_X[self.iVertexExtract]
         Dy = self.nodalDisp_Y[self.iVertexExtract]
@@ -231,8 +294,12 @@ class GetDPSolver(SolidSolver):
         Vx = self.nodalVel_X[self.iVertexExtract]
         Vy = self.nodalVel_Y[self.iVertexExtract]
         Vz = self.nodalVel_Z[self.iVertexExtract]
+        T = self.nodalTemperature[self.iVertexExtract]
+        Qx = self.nodalHeatFlux_X[self.iVertexExtract]
+        Qy = self.nodalHeatFlux_Y[self.iVertexExtract]
+        Qz = self.nodalHeatFlux_Z[self.iVertexExtract]
 
-        solFile.write(str(time) + '\t' + str(nFSIIter) + '\t' + str(Dx) + '\t' + str(Dy) + '\t' + str(Dz) + '\t' + str(Vx) + '\t' + str(Vy) + '\t' + str(Vz) + '\n')
+        solFile.write(str(time) + '\t' + str(nFSIIter) + '\t' + str(Dx) + '\t' + str(Dy) + '\t' + str(Dz) + '\t' + str(Vx) + '\t' + str(Vy) + '\t' + str(Vz) + '\t' + str(T) + '\t' + str(Qx) + '\t' + str(Qy) + '\t' + str(Qz) + '\n')
 
         solFile.close()
 

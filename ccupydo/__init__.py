d14c4a290044bba2426495d979fd064b0253980d
tobadavid <tdavid91@hotmail.com>
2017-09-19 17:14:45 +0200

    Add __init__ to the ccupydo directory.

---
+++ ccupydo/__init__.py
@@ -0,0 +1,11 @@
+# Initialize the CCupydo module
+#
+# Authors : D. THOMAS
+#
+# COPYRIGHT (C) University of Liege, 2017.
+
+from CCupydo import CInterpolator
+from CCupydo import CManager
+from CCupydo import CInterfaceMatrix
+from CCupydo import CFlexInterfaceData
+from CCupydo import CLinearSolver

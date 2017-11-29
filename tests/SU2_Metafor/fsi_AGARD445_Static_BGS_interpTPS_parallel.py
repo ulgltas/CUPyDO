fb9344b5f719ff3b24089a36355b729ee1615d96
tobadavid <tdavid91@hotmail.com>
2017-11-29 18:23:44 +0100

    Add new tests in the battery (3D, non-matching meshes and pseudo-parallel IQN)

---
+++ tests/SU2_Metafor/fsi_AGARD445_Static_BGS_interpTPS_parallel.py
@@ -0,0 +1,123 @@
+import os, sys
+
+filePath = os.path.abspath(os.path.dirname(sys.argv[0]))
+fileName = os.path.splitext(os.path.basename(__file__))[0]
+FSICouplerPath = os.path.join(os.path.dirname(__file__), '../../')
+#appsPath = os.path.join(os.path.dirname(__file__), '../../apps')
+
+sys.path.append(FSICouplerPath)
+#sys.path.append(appsPath)
+
+from math import *
+from optparse import OptionParser
+import FSICoupler as FSI
+
+def getParameters(_p):
+    # --- Input parameters --- #
+    p = {}
+    p['nDim'] = 3
+    p['tollFSI'] = 1e-6
+    p['dt'] = 0.0
+    p['tTot'] = 0.05
+    p['nFSIIterMax'] = 4
+    p['timeIterTreshold'] = -1
+    p['omegaMax'] = 1.0
+    p['computationType'] = 'steady'
+    p['mtfSaveAllFacs'] = False
+    p['nodalLoadsType'] = 'force'
+    p['nZones_SU2'] = 0
+    p['withMPI'] = True
+    p.update(_p)
+    return p 
+
+def main(_p, nogui):
+
+    p = getParameters(_p)
+
+    comm = None
+    myid = 0
+    numberPart = 0
+    rootProcess = 0
+
+    FSI.load(fileName, p['withMPI'], comm, myid, numberPart)
+
+    if p['withMPI']:
+      from mpi4py import MPI
+      comm = MPI.COMM_WORLD
+      myid = comm.Get_rank()
+      numberPart = comm.Get_size()
+    else:
+      comm = None
+      myid = 0
+      numberPart = 1
+
+    # --- Input parameters --- #
+    CFD_file = '../../../tests/SU2_Metafor/AGARD445_Static_SU2Conf.cfg'
+    CSD_file = 'AGARD445_Static_MetaforConf'
+
+    # --- Initialize the fluid solver --- #
+    import SU2Interface
+    if comm != None:
+        FluidSolver = SU2Interface.SU2Solver(CFD_file, p['nZones_SU2'], p['nDim'], p['computationType'], p['nodalLoadsType'], p['withMPI'], comm)
+    else:
+        FluidSolver = SU2Interface.SU2Solver(CFD_file, p['nZones_SU2'], p['nDim'], p['computationType'], p['nodalLoadsType'], p['withMPI'], 0)
+    FSI.mpiBarrier(comm)
+
+    # --- Initialize the solid solver --- #
+    SolidSolver = None
+    if myid == rootProcess:
+        import MtfInterface
+        SolidSolver = MtfInterface.MtfSolver(CSD_file, p['computationType'])
+        SolidSolver.saveAllFacs = p['mtfSaveAllFacs']
+    FSI.mpiBarrier(comm)
+
+    # --- Initialize the FSI manager --- #
+    manager = FSI.Manager(FluidSolver, SolidSolver, p['nDim'], p['computationType'], comm)
+    FSI.mpiBarrier()
+
+    # --- Initialize the interpolator --- #
+    interpolator = FSI.TPSInterpolator(manager, FluidSolver, SolidSolver, comm)
+    solverList = interpolator.getLinearSolvers()
+    for ii in range(2):
+        solverList[ii].setMaxNumberIterations(1000)
+        solverList[ii].setPreconditioner("JACOBI")
+
+    # --- Initialize the FSI criterion --- #
+    criterion = FSI.DispNormCriterion(p['tollFSI'])
+    FSI.mpiBarrier()
+
+    # --- Initialize the FSI algorithm --- #
+    algorithm = FSI.AlgorithmBGSStaticRelax(manager, FluidSolver, SolidSolver, interpolator, criterion, p['nFSIIterMax'], p['dt'], p['tTot'], p['timeIterTreshold'], p['omegaMax'], comm)
+
+    # --- Launch the FSI computation --- #
+    algorithm.run()
+  
+    # --- Exit computation --- #
+    del manager
+    del criterion
+    del FluidSolver
+    del SolidSolver
+    del interpolator
+    del algorithm
+    FSI.mpiBarrier(comm)
+    return 0
+
+
+# -------------------------------------------------------------------
+#  Run Main Program
+# -------------------------------------------------------------------
+
+# --- This is only accessed if running from command prompt --- #
+if __name__ == '__main__':
+
+    p = {}
+    
+    parser=OptionParser()
+    parser.add_option("--nogui", action="store_true",
+                        help="Specify if we need to use the GUI", dest="nogui", default=False)
+
+    (options, args)=parser.parse_args()
+    
+    nogui = options.nogui
+    
+    main(p, nogui)

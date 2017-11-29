dd71d5f27fac9be0e6592b02a4ad5bd4c5ec6b07
tobadavid <tdavid91@hotmail.com>
2017-11-29 17:03:18 +0100

    Add a coupling interface for the modal interpreter.

---
+++ ModalInterface.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: latin-1; -*-
+
+# SU2Interface.py
+# Python interface between the modal data base and CUPyDO.
+# Authors D. THOMAS
+#
+# COPYRIGHT (C) University of Liège, 2017.
+
+# ----------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------
+
+import numpy as np
+from FSICoupler import SolidSolver
+from math import *
+
+# ----------------------------------------------------------------------
+#  Modal solver interface class
+# ----------------------------------------------------------------------
+
+class modalInterpreter(SolidSolver):
+    """
+    Description
+    """
+
+    def __init__(self, confFile, freq):
+        """
+        Des.
+        """
+
+        print("\n***************************** Initialize modal interpreter *****************************\n")
+
+        self.modalDataFileName = confFile
+        self.frequency = freq
+        self.interfaceID = None
+        self.nNodes = self.__linecount(self.modalDataFileName)-1
+        self.nHaloNode = 0
+        self.nPhysicalNodes = self.nNodes
+
+        self.nodalMod_X = np.zeros(self.nPhysicalNodes, dtype=float)
+        self.nodalMod_Y = np.zeros(self.nPhysicalNodes, dtype=float)
+        self.nodalMod_Z = np.zeros(self.nPhysicalNodes, dtype=float)
+
+        self.nodalDisp_X = np.zeros(self.nPhysicalNodes, dtype=float)
+        self.nodalDisp_Y = np.zeros(self.nPhysicalNodes, dtype=float)
+        self.nodalDisp_Z = np.zeros(self.nPhysicalNodes, dtype=float)
+
+        self.nodalCoord_X = np.zeros(self.nPhysicalNodes, dtype=float)
+        self.nodalCoord_Y = np.zeros(self.nPhysicalNodes, dtype=float)
+        self.nodalCoord_Z = np.zeros(self.nPhysicalNodes, dtype=float)
+
+        self.nodalGlobalIndex = np.zeros(self.nPhysicalNodes, dtype=int)
+
+        self.haloNodeList = {}
+
+        self.__readModalData()
+
+    def __readModalData(self):
+        """
+        Des.
+        """
+
+        print('Counted modal data for {} nodes.'.format(self.nPhysicalNodes))
+        iVertex = 0
+
+        with open(self.modalDataFileName, 'r') as modalDataFile:
+            print('Opened modal data file ' + self.modalDataFileName + '.')
+            line = modalDataFile.readline()
+            if line:
+                pos = line.find('Global_Index')
+                if pos == -1:
+                    raise Exception("ERROR")
+                while 1:
+                    line = modalDataFile.readline()
+                    if not line:
+                        break
+                    line = line.split(',')
+                    for ii in range(len(line)):
+                        line[ii] = line[ii].strip(' ')
+                        line[ii] = line[ii].strip('\n\r')
+                    self.nodalGlobalIndex[iVertex] = int(line[0])
+                    self.nodalCoord_X[iVertex] = float(line[1])
+                    self.nodalCoord_Y[iVertex] = float(line[2])
+                    self.nodalCoord_Z[iVertex] = float(line[3])
+                    self.nodalMod_X[iVertex] = float(line[4])
+                    self.nodalMod_Y[iVertex] = float(line[5])
+                    self.nodalMod_Z[iVertex] = float(line[6])
+                    iVertex += 1
+
+        if iVertex != self.nPhysicalNodes:
+            raise("ERROR")
+        else:
+            print('Read modal data for {} nodes.'.format(iVertex))
+
+    def __linecount(self, fileName):
+        """
+        Des
+        """
+
+        count = 0
+
+        with open(fileName, 'r') as thefile:
+            while 1:
+                line = thefile.readline()
+                if not line:
+                    break
+                count += 1
+
+        return count
+
+    def run(self, t1, t2):
+        """
+        Des.
+        """
+
+        mult = sin(2*3.141592*self.frequency*t2)
+
+        self.nodalDisp_X = self.nodalMod_X*mult
+        self.nodalDisp_Y = self.nodalMod_Y*mult
+        self.nodalDisp_Z = self.nodalMod_Z*mult
+
+    def getNodalInitialPositions(self):
+        """
+        Des.
+        """
+
+        return (self.nodalCoord_X, self.nodalCoord_Y, self.nodalCoord_Z)
+
+    def getNodalIndex(self, iVertex):
+        """
+        Des.
+        """
+
+        if iVertex >= self.nPhysicalNodes or iVertex < 0:
+            raise Exception('INDEX OUT OF BOUND.')
+        else:
+            return self.nodalGlobalIndex[iVertex]
+
+    def exit(self):
+        """
+        Des.
+        """
+
+        print("***************************** Exit modal interpreter *****************************")
+
+

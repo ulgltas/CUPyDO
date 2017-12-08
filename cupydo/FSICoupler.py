1793f03eb781df45cdac7ca16ae503971707a544
tobadavid <tdavid91@hotmail.com>
2017-12-08 12:48:56 +0100

    Modify FSICoupler split linearSolver.

--- cupydo/FSICoupler.py
+++ cupydo/FSICoupler.py
@@ -36,45 +36,6 @@ _theWDir    = None # workspace directory
 _theWDirRoot = os.getcwd()  # base directory du workspace
 
 # ----------------------------------------------------------------------
-#  Linear solver class
-# ----------------------------------------------------------------------
-
-class LinearSolver(ccupydo.CLinearSolver):
-    """
-    Define a parallel linear solver (serial compatible).
-    Designed to be used with InterfaceData and InterfaceMatrix classes.
-    Inherited public members :
-        -solve()
-    """
-
-    def __init__(self, MatrixOperator, mpiComm=None):
-        """
-        Constructor.
-        MatrixOperator is of type InterfaceMatrix
-        """
-
-        ccupydo.CLinearSolver.__init__(self, MatrixOperator)
-
-        self.mpiComm = mpiComm
-
-        if mpiComm == None:
-            self.LinOperator = MatrixOperator.getMat()
-
-    def solve(self, DataB, DataX):
-        """
-        Solve system MatrixOperator*VecX = VecB.
-        VecX and VecB are InterfaceData types.
-        """
-
-        if self.mpiComm != None:
-            ccupydo.CLinearSolver.solve(self, DataB, DataX)
-        else:
-            dim = DataB.getDim()
-            for iDim in range(dim):
-                XX = splinalg.spsolve(self.LinOperator, DataB.getData(iDim))
-                DataX.setData(iDim, XX)
-
-# ----------------------------------------------------------------------
 #  Generic solid solver class
 # ----------------------------------------------------------------------
 

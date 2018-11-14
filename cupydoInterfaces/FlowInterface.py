f0f5fde24475eace5a1239cba3205bbab178a1e9
acrovato <39187559+acrovato@users.noreply.github.com>
2018-11-14 17:43:54 +0100

    Clnd tab. Updt load computation

--- cupydoInterfaces/FlowInterface.py
+++ cupydoInterfaces/FlowInterface.py
@@ -33,20 +33,23 @@ from cupydo.genericSolvers import FluidSolver
 
 class FlowSolver(FluidSolver):
     def __init__(self, case, bndid):
-		self.case = case         # name of the module of the fluid case
+        self.case = case         # name of the module of the fluid case
         self.bndid = bndid       # id of physical group of the f/s interface
-		
-		# load the python module
-		import self.case as module
-		
-		# create an instance of Flow
-        self.flow = module.getFlow() #TODO
+        
+        # load the python module
+        import self.case as module
+        
+        # create an instance of Flow
+        # TODO: flow members used in this file that should be pointed to with getFlow()
+        # self.flow.solver.phi
+        # self.flow.solver.execute()
+        self.flow = module.getFlow()
         
         # create an object handling the f/s boundary
         self.boundary = self.flow.Boundary(self.flow.msh, bndid)
 
-		# number of nodes
-		self.nNodes = self.boundary.nodes.size()
+        # number of nodes
+        self.nNodes = self.boundary.nodes.size()
         self.nHaloNode = 0
         self.nPhysicalNodes = self.nNodes - self.nHaloNode
 
@@ -55,51 +58,54 @@ class FlowSolver(FluidSolver):
         FluidSolver.__init__(self)
         # TODO should nodal positions and loads be initialized here too?
         
-	def run(self, t1, t2):
+    def run(self, t1, t2):
         """
         Run the solver for one steady (time) iteration.
         """
         self.exeOK = self.flow.Solver.execute()
         self.__setCurrentState()
-	
-	def __setCurrentState(self):
-		"""
-		Compute nodal forces from nodal Pressure coefficient
-		"""
-	
-		# Dynamic pressure
-		p = 0.5 #0.5 * rho * U_inf.norm()*U_inf.norm() #TODO
-		
-		# elemental forces: integrate pressure from nodes
-		fxE = {}
+    
+    def __setCurrentState(self):
+        """
+        Compute nodal forces from nodal Pressure coefficient
+        """
+    
+        # Flow definition TODO
+        p = 0.5 #0.5 * rho * U_inf.norm()*U_inf.norm() #TODO
+        M_inf = 0.3
+        gamma = 1.4
+        
+        # elemental forces: integrate pressure from nodal potential
+        if  M_inf == 0:
+            CfE = airfoil.integrate(self.flow.solver.phi, self.flow.Fun0EleCpL())
+        else:
+            CfE = airfoil.integrate(self.flow.solver.phi, self.flow.Fun0EleCp(gamma, M_inf))
+        fxE = {}
         fyE = {}
         fzE = {}
-		for e in self.boundary.elems:
-			cp = 0
-			for n in e.nodes:
-				cp += self.flow.Solver.Cp[n.row]
-			cp /= e.nodes.size()
-			sur = e.computeV()
-			fxE[e] = p * cp * e.normal().x[0] * sur #TODO
-			fyE[e] = p * cp * e.normal().x[1] * sur
-			fzE[e] = p * cp * e.normal().x[2] * sur
+        i = 0
+        for e in self.boundary.tag.elems:
+            fxE[e] = p * cfE[i] * e.normal().x[0]
+            fyE[e] = p * cfE[i] * e.normal().x[1]
+            fzE[e] = p * cfE[i] * e.normal().x[2]
+            i += 1
 
-		# nodal forces: average forces from elements
-		i = 0
+        # nodal forces: average forces from elements
+        i = 0
         for n in self.boundary.nodes:
-			for e in self.boundary.neMap[n]:
-				fx += fxE.get(e)
-				fy += fyE.get(e)
-				fz += fzE.get(e)
-			self.nodalLoad_X[i] = fx / self.boundary.neMap[n].size()
-			self.nodalLoad_Y[i] = fy / self.boundary.neMap[n].size()
-			self.nodalLoad_Z[i] = fz / self.boundary.neMap[n].size()
-			i += 1
+            for e in self.boundary.neMap[n]:
+                fx += fxE.get(e)
+                fy += fyE.get(e)
+                fz += fzE.get(e)
+            self.nodalLoad_X[i] = fx / self.boundary.neMap[n].size()
+            self.nodalLoad_Y[i] = fy / self.boundary.neMap[n].size()
+            self.nodalLoad_Z[i] = fz / self.boundary.neMap[n].size()
+            i += 1
 
     def getNodalInitialPositions(self):
-    	"""
-    	Get the initial position of each node
-    	"""
+        """
+        Get the initial position of each node
+        """
         
         x0 = np.zeros(self.nPhysicalNodes)
         y0 = np.zeros(self.nPhysicalNodes)
@@ -120,7 +126,7 @@ class FlowSolver(FluidSolver):
         no = self.boundary.nodes[iVertex].no
         return no
 
-	def applyNodalDisplacements(self, disp_X, disp_Y, disp_Z):
+    def applyNodalDisplacements(self, disp_X, disp_Y, disp_Z):
         """
         Apply displacements coming from solid solver to f/s interface
         """
@@ -131,9 +137,9 @@ class FlowSolver(FluidSolver):
             self.boundary.nodes[i].pos.x[2] += disp_Z[i]
 
     def remeshing(self):
-    	"""
-    	TODO Lagrangian remeshing
-    	"""
+        """
+        TODO Lagrangian remeshing
+        """
 
     def fakeSolidSolver(self):
         """
@@ -145,16 +151,16 @@ class FlowSolver(FluidSolver):
         dyD = np.zeros(self.nPhysicalNodes)
         dzD = np.zeros(self.nPhysicalNodes)
         self.applyNodalDisplacements(self, dxD, dyD, dzD) # TODO: check 'self' parameter           
-		
+        
     def update(self):
-    	"""
-    	TODO
-    	"""
+        """
+        TODO
+        """
         
     def save(self):
-    	"""
-    	TODO
-    	"""
+        """
+        TODO
+        """
 
     def initRealTimeData(self):
         """
@@ -173,7 +179,7 @@ class FlowSolver(FluidSolver):
         
         #print toPrint
     
-	def exit(self):
+    def exit(self):
         """
         Exit the Flow solver.
         """

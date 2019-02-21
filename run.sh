fbabefb3585d97b1a14542770a2aed3c112f7c72
acrovato <39187559+acrovato@users.noreply.github.com>
2019-02-21 18:28:00 +0100

    Corr run.sh

--- run.sh
+++ run.sh
@@ -4,20 +4,21 @@
 
 . ~/.profile
 
-export MTF_RUN="../Metafor"
-export SU2_RUN="../SU2/bin"
+export TOP_DIR=$(cd "$(dirname "$0")/.."; pwd)
+export MTF_RUN="${TOP_DIR}/Metafor"
+export SU2_RUN="${TOP_DIR}/SU2/bin"
 # PATH
 export PATH=$PATH:${SU2_RUN}
 # LIB
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MTF_RUN}
 # PYTHON
-export PYTHONPATH="${PYTHONPATH}:${SU2_RUN}"
-export PYTHONPATH="${PYTHONPATH}:../waves"
-export PYTHONPATH="${PYTHONPATH}:../waves/build/bin"
-export PYTHONPATH="${PYTHONPATH}:../NativeSolid/bin"
+export PYTHONPATH=${PYTHONPATH}:${SU2_RUN}
+export PYTHONPATH="${PYTHONPATH}:${TOP_DIR}/waves"
+export PYTHONPATH="${PYTHONPATH}:${TOP_DIR}/waves/build/bin"
+export PYTHONPATH="${PYTHONPATH}:${TOP_DIR}/NativeSolid/bin"
 export PYTHONPATH=${PYTHONPATH}:${MTF_RUN}
 
 echo ${PATH}
 echo ${LD_LIBRARY_PATH}
-echo ${PYTHON_PATH}
+echo ${PYTHONPATH}
 

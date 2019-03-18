591b5524aa9c51d1bbb19f77a0f299e5079f6a37
Mariano Sánchez Martínez <m.sanchezmartinez@student.uliege.be>
2019-03-18 12:04:34 +0100

    Corrected some encoding issues, changed filenames for NMM testcase mesh

--- tests/SU2_RBM/PitchPlungeN0012_IQN_fsi.py
+++ tests/SU2_RBM/PitchPlungeN0012_IQN_fsi.py
@@ -3,7 +3,7 @@
 
 ''' 
 
-Copyright 2018 University of Liï¿½ge
+Copyright 2018 University of Liège
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

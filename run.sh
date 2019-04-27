10f4dae40c656030b0174d89a2f2e05f4859c9be
acrovato <39187559+acrovato@users.noreply.github.com>
2019-04-27 14:44:13 +0200

    Change run.sh to call ctest (tmp submit script for remote run)

--- run.sh
+++ run.sh
@@ -1,55 +1,21 @@
 #!/bin/bash
-# Run script for CUPyDO
-# External solver dir should place next to CUPyDO dir
+# Run battery script for CUPyDO
+# This is a temporary solution to remotely run the battery
 
 # Load .profile
 . ~/.profile
 
-# Set paths
-CUP_DIR="$(dirname "$0")"
-TOP_DIR=$(cd "${CUP_DIR}/.."; pwd)
-MTF_RUN="${TOP_DIR}/Metafor"
-# LIB
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MTF_RUN}
-# PYTHON
-export PYTHONPATH=${PYTHONPATH}:${CUP_DIR}
-export PYTHONPATH="${PYTHONPATH}:${TOP_DIR}/SU2/bin"
-export PYTHONPATH="${PYTHONPATH}:${TOP_DIR}/waves"
-export PYTHONPATH="${PYTHONPATH}:${TOP_DIR}/NativeSolid/bin"
-export PYTHONPATH=${PYTHONPATH}:${MTF_RUN}
-export PYTHONPATH="${PYTHONPATH}:${TOP_DIR}/ModalSolver/"
-
-# Print paths
-echo PATH=${PATH}
-echo LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
-echo PYTONPATH=${PYTHONPATH}
-echo ""
-
 # Check if arguments are provided
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
   then
-    echo "Usage: ./run.sh f n"
-    echo -e "\tf = path/to/file (run single test) || f = all (run ctest)"
+    echo "Usage: ./run.sh n"
     echo -e "\tn = number of threads"
     echo "ERROR: Wrong number of arguments provided!"
     exit 1
 fi
 
 # Run
-if [ "$1" == "all" ]
-  then
-    echo "Running ctest on $2 threads..."
-    cd build
-    ctest -j $2 2>&1 | tee ${CUP_DIR}/workspace/FSI_ctest.log
-else
-    if [[ -f $1 ]]
-      then
-        echo "Running file $1 on $2 threads..."
-        tdir=${1%.*}
-        python $1 -n $2 2>&1 | tee ${CUP_DIR}/workspace/FSI_${tdir##*/}.log
-    else
-        echo "$1 is not a file"
-        exit 1
-    fi
-fi
+echo "Running ctest on $1 threads..."
+cd build
+ctest -j $1
 

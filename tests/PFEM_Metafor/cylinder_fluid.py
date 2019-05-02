558c5740dddabb644eb5b037476973c89d36d273
Romain Boman <romain.boman@gmail.com>
2019-05-02 10:32:24 +0200

    change test names to a human-readable format

--- tests/PFEM_Metafor/cylinder_fluid.py
+++ tests/PFEM_Metafor/cylinder_fluid.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# -*- coding: latin-1; -*-
+# -*- coding: utf-8 -*-
 
 ''' 
 
@@ -19,7 +19,9 @@ limitations under the License.
 
 '''
 
-import sys, os, os.path
+import sys
+import os
+import os.path
 
 runPath = os.path.dirname(sys.modules[__name__].__file__)
 filePath = os.path.abspath(os.path.dirname(__file__))
@@ -29,31 +31,35 @@ import pfem
 
 import pfemtools as wt
 import viewer as v
-    
+
 w = None
 
+
 class Module:
-    def __init__(self, w, msh, pbl, solScheme, nonLinAlgo, convCriterion, scheme, extManager, gui):
-       self.w = w
-       self.msh = msh
-       self.pbl = pbl
-       self.solScheme = solScheme
-       self.nonLinAlgo = nonLinAlgo
-       self.convCriterion = convCriterion
-       self.scheme = scheme
-       self.extManager = extManager
-       self.gui = gui
+    def __init__(self, w, msh, pbl, solScheme, nonLinAlgo,
+                 convCriterion, scheme, extManager, gui):
+        self.w = w
+        self.msh = msh
+        self.pbl = pbl
+        self.solScheme = solScheme
+        self.nonLinAlgo = nonLinAlgo
+        self.convCriterion = convCriterion
+        self.scheme = scheme
+        self.extManager = extManager
+        self.gui = gui
+
 
 def getPfem():
     global w
-    if w: return w
+    if w:
+        return w
     w = pfem
-    
-    mshFile = runPath+os.sep+'StaticCylinder.msh'
-    
+
+    mshFile = runPath + os.sep + 'cylinder.msh'
+
     rho0 = 1.0
     mu = 0.01
-    
+
     pbl = w.Problem()
     pbl.rho0 = rho0
     pbl.mu = mu
@@ -61,53 +67,49 @@ def getPfem():
     pbl.extP = 1.0
     pbl.scalingU = 1.0
     pbl.bodyForceY = -9.81
-    
+
     msh = w.MshData(pbl)
     msh.load(mshFile)
     print msh
-    
+
     toll = 1e-6
     nItMax = 20
-    
+
     solScheme = w.SchemeMonolithicPSPG(msh, pbl)
     convCriterion = w.ForcesBalanceNormedBodyForceCriterion(msh, pbl, toll)
     nonLinAlgo = w.PicardAlgorithm(solScheme, convCriterion, nItMax)
-    
+
     scheme = w.BackwardEuler(msh, pbl, nonLinAlgo)
-    
+
     msh.ptags[9].name = "Cylinder"
     msh.ntags["Cylinder"] = msh.ptags[9]
-    
+
     w.Medium(msh, 9, 0., 0., 3)
     w.Medium(msh, 14, mu, rho0, 1)
-    
+
     # boundaries
     w.Boundary(msh, 8, 3, pbl.extP)
     w.Boundary(msh, 7, 1, 0.0)
     w.Boundary(msh, 7, 2, 0.0)
     w.Boundary(msh, 9, 1, 0.0)
     w.Boundary(msh, 9, 2, 0.0)
-    
-    scheme.savefreq=1
+
+    scheme.savefreq = 1
     scheme.gamma = 0.6
     scheme.omega = 0.5
     scheme.addRemoveNodesOption = True
-    
-    #Results
+
+    # Results
     extManager = w.ExtractorsManager(msh)
-    extManager.add(1,w.PositionExtractor(msh,6))
-    extManager.add(20,w.IntForceExtractor(msh,"Cylinder"))
-    extManager.add(21,wt.TotalIntForceExtractor(msh,"Cylinder"))
-    
-    gui = v.MeshViewer(msh, scheme, True) 
-    
+    extManager.add(1, w.PositionExtractor(msh, 6))
+    extManager.add(20, w.IntForceExtractor(msh, "Cylinder"))
+    extManager.add(21, wt.TotalIntForceExtractor(msh, "Cylinder"))
+
+    gui = v.MeshViewer(msh, scheme, True)
+
     return Module(w, msh, pbl, solScheme, nonLinAlgo, convCriterion, scheme, extManager, gui)
-    
+
+
 def getRealTimeExtractorsList(pfem):
-    
     extractorsList = []
-
-    # --- Extractors list starts --- #
-    # --- Extractors list ends --- #
-
     return extractorsList
--- tests/PFEM_Metafor/cylinder_fsi.py
+++ tests/PFEM_Metafor/cylinder_fsi.py
@@ -1,4 +1,8 @@
-import os, sys
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
 
 filePath = os.path.abspath(os.path.dirname(__file__))
 fileName = os.path.splitext(os.path.basename(__file__))[0]
@@ -16,8 +20,8 @@ import cupydo.algorithm as cupyalgo
 import numpy as np
 from cupydo.testing import *
 
+
 def getParameters(_p):
-    # --- Input parameters --- #
     p = {}
     p['nthreads'] = 1
     p['nDim'] = 2
@@ -33,8 +37,9 @@ def getParameters(_p):
     p.update(_p)
     return p
 
-def main(_p, nogui): # NB, the argument 'nogui' is specific to PFEM only!
-    
+
+def main(_p, nogui):  # NB, the argument 'nogui' is specific to PFEM only!
+
     # --- Get FSI parameters ---#
     p = getParameters(_p)
 
@@ -42,59 +47,69 @@ def main(_p, nogui): # NB, the argument 'nogui' is specific to PFEM only!
     withMPI, comm, myid, numberPart = cupyutil.getMpi()
     rootProcess = 0
     #cupyutil.load(filePath, fileName, withMPI, comm, myid, numberPart)
-    
+
     # --- Input parameters --- #
-    cfd_file = 'StaticCylinder_fluid_Pfem'
-    csd_file = 'StaticCylinder_cylinder_Mtf'
-    
+    cfd_file = 'cylinder_fluid'
+    csd_file = 'cylinder_solid'
+
     # --- Initialize the fluid solver --- #
     import cupydoInterfaces.PfemInterface
-    fluidSolver = cupydoInterfaces.PfemInterface.PfemSolver(cfd_file, 9, p['dt'])
-    
+    fluidSolver = cupydoInterfaces.PfemInterface.PfemSolver(
+        cfd_file, 9, p['dt'])
+
     # --- This part is specific to PFEM ---
     fluidSolver.pfem.scheme.nthreads = p['nthreads']
     fluidSolver.pfem.scheme.savefreq = p['saveFreqPFEM']
     if nogui:
         fluidSolver.pfem.gui = None
     # ---
-    
+
     cupyutil.mpiBarrier(comm)
-    
+
     # --- Initialize the solid solver --- #
     solidSolver = None
     if myid == rootProcess:
         import cupydoInterfaces.MtfInterface
-        solidSolver = cupydoInterfaces.MtfInterface.MtfSolver(csd_file, p['computationType'])
-        
+        solidSolver = cupydoInterfaces.MtfInterface.MtfSolver(
+            csd_file, p['computationType'])
+
         # --- This part is specific to Metafor ---
         solidSolver.saveAllFacs = p['mtfSaveAllFacs']
-        
+
     cupyutil.mpiBarrier(comm)
-        
+
     # --- Initialize the FSI manager --- #
-    manager = cupyman.Manager(fluidSolver, solidSolver, p['nDim'], p['computationType'], comm)
+    manager = cupyman.Manager(fluidSolver, solidSolver,
+                              p['nDim'], p['computationType'], comm)
     cupyutil.mpiBarrier()
 
     # --- Initialize the interpolator --- #
-    interpolator = cupyinterp.MatchingMeshesInterpolator(manager, fluidSolver, solidSolver, comm)
-    
+    interpolator = cupyinterp.MatchingMeshesInterpolator(
+        manager, fluidSolver, solidSolver, comm)
+
     # --- Initialize the FSI criterion --- #
     criterion = cupycrit.DispNormCriterion(p['tollFSI'])
     cupyutil.mpiBarrier()
-    
+
     # --- Initialize the FSI algorithm --- #
-    algorithm = cupyalgo.AlgorithmBGSAitkenRelax(manager, fluidSolver, solidSolver, interpolator, criterion, p['nFSIIterMax'], p['dt'], p['tTot'], p['timeIterTreshold'], p['omegaMax'], comm)
-    
+    algorithm = cupyalgo.AlgorithmBGSAitkenRelax(manager, fluidSolver,
+                                                 solidSolver, interpolator,
+                                                 criterion, p['nFSIIterMax'],
+                                                 p['dt'], p['tTot'],
+                                                 p['timeIterTreshold'], p['omegaMax'],
+                                                 comm)
+
     # --- Launch the FSI computation --- #
     algorithm.run()
 
     # --- Check the results --- #
-    
+
     # Check convergence and results
     if (algorithm.errValue > p['tollFSI']):
         print "\n\n" + "FSI residual = " + str(algorithm.errValue) + ", FSI tolerance = " + str(p['tollFSI'])
-        raise Exception(ccolors.ANSI_RED + "FSI algo failed to converge!" + ccolors.ANSI_RESET)
-    
+        raise Exception(ccolors.ANSI_RED +
+                        "FSI algo failed to converge!" + ccolors.ANSI_RESET)
+
     # Read results from file
     with open("Node_6_POS.ascii", 'rb') as f:
         lines = f.readlines()
@@ -102,31 +117,35 @@ def main(_p, nogui): # NB, the argument 'nogui' is specific to PFEM only!
     with open("TOTAL_F_INT_Cylinder.ascii", 'rb') as f:
         lines = f.readlines()
     result_2 = np.genfromtxt(lines[-1:], delimiter=None)
-    
+
     tests = CTests()
-    tests.add(CTest('Mean nb of FSI iterations', algorithm.getMeanNbOfFSIIt(), 3, 1, True)) # abs. tol. of 1
-    tests.add(CTest('Y-coordinate Node 6', result_1[1], 0.25, 1e-4, False)) # rel. tol. of 0.001%
-    tests.add(CTest('Total force on cylinder', result_2[1], -0.116, 1e-2, False)) # rel. tol. of 1%
+    tests.add(CTest('Mean nb of FSI iterations',
+                    algorithm.getMeanNbOfFSIIt(), 3, 1, True))  # abs. tol. of 1
+    tests.add(CTest('Y-coordinate Node 6',
+                    result_1[1], 0.25, 1e-4, False))  # rel. tol. of 0.001%
+    tests.add(CTest('Total force on cylinder',
+                    result_2[1], -0.116, 1e-2, False))  # rel. tol. of 1%
     tests.run()
 
 # -------------------------------------------------------------------
 #    Run Main Program
 # -------------------------------------------------------------------
 
+
 # --- This is only accessed if running from command prompt --- #
 if __name__ == '__main__':
-    
+
     p = {}
-    
-    parser=OptionParser()
+
+    parser = OptionParser()
     parser.add_option("--nogui", action="store_true",
-                        help="Specify if we need to use the GUI", dest="nogui", default=False)
-    parser.add_option("-n", type="int", help="Number of threads", dest="nthreads", default=1)
-    
-    
-    (options, args)=parser.parse_args()
-    
+                      help="Specify if we need to use the GUI", dest="nogui", default=False)
+    parser.add_option("-n", type="int", help="Number of threads",
+                      dest="nthreads", default=1)
+
+    (options, args) = parser.parse_args()
+
     nogui = options.nogui
     p['nthreads'] = options.nthreads
-    
+
     main(p, nogui)

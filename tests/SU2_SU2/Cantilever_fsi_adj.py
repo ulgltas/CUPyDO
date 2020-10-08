c7e6b1b7d53e878d2aff03dcad055cdd1c538ee6
Mariano Sánchez Martínez <m.sanchezmartinez@student.uliege.be>
2020-10-08 15:37:54 +0200

    Change extractors in adjoint

--- tests/SU2_SU2/Cantilever_fsi_adj.py
+++ tests/SU2_SU2/Cantilever_fsi_adj.py
@@ -32,11 +32,6 @@ def test(res, tol):
         lines = f.readlines()
     resultS = np.genfromtxt(lines[-1:], delimiter=None)
 
-    # Check convergence and results
-    # residual for test is 0.002
-    #if (res > tol):
-    #    print "\n\n" + "FSI residual = " + str(res) + ", FSI tolerance = " + str(tol)
-    #    raise Exception(ccolors.ANSI_RED + "FSI algo failed to converge!" + ccolors.ANSI_RESET)
     tests = CTests()
     tests.add(CTest('Lift coefficient', resultA[2], -0.425, 1e-1, False)) # rel. tol. of 10%
     tests.add(CTest('Drag coefficient', resultA[3], 3.124, 1e-1, False)) # rel. tol. of 10%
@@ -71,7 +66,7 @@ def getAdjP():
     p['rbfRadius'] = .3
     p['nodalLoadsType'] = 'force'
     # Solid parameters
-    p['extractors'] = [100, 110]
+    p['extractors'] = [2, 3]
     return p
 
 def main():

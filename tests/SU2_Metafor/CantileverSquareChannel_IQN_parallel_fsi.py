dcaf99d37771f04cd85296b366274c449c0d8ca2
Martin Lacroix <lacroix.martin@tutanota.com>
2023-07-01 11:05:13 +0200

    Update Reference Test Cases

--- tests/SU2_Metafor/CantileverSquareChannel_IQN_parallel_fsi.py
+++ tests/SU2_Metafor/CantileverSquareChannel_IQN_parallel_fsi.py
@@ -37,7 +37,7 @@ def test(res, tol):
     tests = CTests()
     tests.add(CTest('Lift coefficient', resultA[2], 0.00029, 1e-1, False))
     tests.add(CTest('Drag coefficient', resultA[3], 2.067434, 1e-1, False))
-    tests.add(CTest('Displacement (104, TY)', resultS[-1], 0., 1e-4, True))
+    tests.add(CTest('Displacement (104, TY)', resultS[-1], 0.0, 1e-4, True))
     tests.run()
 
 def getFsiP():

5503a1fca3da63e49da260bd5e45a34a3d02cf08
Martin Lacroix <lacroix.martin@tutanota.com>
2023-10-26 17:10:36 +0200

    Update to New PFEM3D Master

--- tests/PFEM3D_Metafor/coolingDisk/input_fsi.py
+++ tests/PFEM3D_Metafor/coolingDisk/input_fsi.py
@@ -21,9 +21,9 @@ def test(meanFSIIt):
     if gmsh.isInitialized(): gmsh.finalize()
 
     tests = CTests()
-    tests.add(CTest('Center ball coordinate X', coord[0], 0.168477, 1e-3, False))
-    tests.add(CTest('Center ball coordinate Y', coord[1], 0.176114, 0.01, False))
-    tests.add(CTest('Center ball temperature', temperature, 170.246805, 0.005, False))
+    tests.add(CTest('Center ball coordinate X', coord[0], 0.162837, 0.01, False))
+    tests.add(CTest('Center ball coordinate Y', coord[1], 0.181850, 0.01, False))
+    tests.add(CTest('Center ball temperature', temperature, 169.707391, 0.005, False))
     tests.add(CTest('Mean number of ISI iterations', meanFSIIt, 5, 1, True))
     tests.run()
 
